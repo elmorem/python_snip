252e146575fde6f509b97cf6a93edd971613b3c1
elmorem <elmorem@gmail.com>
2020-03-03 10:56:21 -0800

    generator

--- sandbox.py
+++ sandbox.py
@@ -4,3 +4,10 @@ new_list = [x**2 for x in list]
 
 print(new_list)
 
+ #square is a generator
+square = (i*i for i in irange(1000000))
+#add the squares
+total = 0
+for i in square:
+    total += i
+print(total)
