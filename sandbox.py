0ddf6ad8691e6e4f75b195dade54bfb75e4ef4b3
elmorem <elmorem@gmail.com>
2020-06-29 09:35:00 -0700

    test

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,17 @@
-list= [2,4,6,8,1,3,5,7,9]
+l1= [2,4,6,8,1,3,5,7,9]
+l2=[4,6,3,6,9,5,3,66,22]
 
-new_list = [x**2 for x in list]
 
-print(new_list)
+new_list=zip(l1, l2)
+print(list(new_list))
+
+map_list= map()
 
  #square is a generator
-square = (i*i for i in irange(1000000))
-#add the squares
-total = 0
-for i in square:
-    total += i
-print(total)
+# square = (i*i for i in range(1000000))
+# #add the squares
+# total = 0
+# for i in square:
+#     total += i
+# print(total)
+
---
+++ test.py
@@ -0,0 +1,3 @@
+x=sum([x*x for x in [1,2,3]])
+
+def_f(n):
